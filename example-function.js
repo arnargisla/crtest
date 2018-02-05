1325a313eb7b6d257bd6dfd825e8bb795e420be0
Arnar Þór Gíslason <arnargisla@gmail.com>
2018-02-05 15:59:46 +0100

    Add display status feature

---
+++ example-function.js
@@ -0,0 +1,14 @@
+function displayStatus() {
+  const appElement = document.querySelector("#app");
+  appElement.innerHTML = '';
+
+  appendStatusLineToElement("Status1: example status message1", appElement);
+  appendStatusLineToElement("Status2: example status message2", appElement);
+  appendStatusLineToElement("Status3: example status message3", appElement);
+}
+
+function appendStatusLineToElement(text, element){
+  const divElement = document.createElement("div");
+  divElement.textContent = text;
+  element.appendChild(divElement)
+}

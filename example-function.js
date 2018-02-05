59a6a77f75491b45937043ec0a0e1a4d4086cc37
arnargisla <arnargisla@gmail.com>
2018-02-05 15:33:32 +0100

    Extract function refactoring

--- example-function.js
+++ example-function.js
@@ -2,19 +2,13 @@ function displayStatus() {
   var appElement = document.querySelector("#app");
   appElement.innerHTML = '';
 
-  var status1 = "example status message1";
-  var el1 = document.createElement("div");
-  el1.textContent = "Status1: " + status1;
-  appElement.appendChild(el1)
-
-  var status2 = "example status message2";
-  var el2 = document.createElement("div");
-  el2.textContent = "Status2: " + status2;
-  appElement.appendChild(el2)
-
-  var status3 = "example status message3";
-  var el3 = document.createElement("div");
-  el3.textContent = "Status3: " + status3;
-  appElement.appendChild(el3)
+  appendStatusLineToElement("Status1: example status message1", appElement);
+  appendStatusLineToElement("Status2: example status message2", appElement);
+  appendStatusLineToElement("Status3: example status message3", appElement);
 }
 
+function appendStatusLineToElement(text, element){
+  const divElement = document.createElement("div");
+  divElement.textContent = text;
+  element.appendChild(divElement)
+}
